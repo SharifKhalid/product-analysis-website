52fb913c3ff408f601bcf679fb2573fbf848d25c
SharifKhalid <sharifkhalid161097@gmail.com>
2022-04-05 23:02:11 +0600

    final

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -10,7 +10,10 @@ const Blogs = () => {
                     Inline element  ও Block element. Block element browser এ নতুন লাইন থেকে শুরু হয়। এবং সম্পূর্ণ width টুকুই নিয়ে নেয়। Inline element নতুন লাইন থেকে শুরু হয় না। এবং সম্পূর্ণ width নিয়ে নেয় না। যতটুকু প্রয়োজন ঠিক ততটুকুই নেয়। 
                     আবার Inline element কে Inline block এ রূপান্তর করা যায়। তখন একে width এবং heightও দেয়া যায়। তবে Block element এর সাথে Inline block এর পার্থক্য আছে। তা হলো Inline block নতুন লাইন থেকে শুরু হয় না। এটি Inline ও block উভয়ের মতই আচরণ করে।  </p>
             </div>
-            <div></div>
+            <div className='bonus'>
+                <h2>Semantic Tag</h2>
+                <p>Semantic Tag, html tag সম্পর্কে একই সাথে Browser কে  এবং একজন web developer কেও স্পষ্ট একটা ধারনা দেয়। যেমন- div tag এর মধ্যে একটা article ও লেখা যায়, header ও দেয়া যায়। আবার section হিসাবও ব্যবহার করা যায়। ফলে web developerদের বুঝতে সমস্যা হয়। এজন্যই Semantic Tag ব্যবহার করা হয়। যেমন- nav, article, section. যাতে করে web developer আলাদা করে বুঝতে পারে কোনটা কেনো ব্যবহার হয়েছে।    </p>
+            </div>
         </div>
     );
 };
