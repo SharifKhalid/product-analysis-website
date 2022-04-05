0aed8a7a65fb0fc2e294eb0cf41848740cd8bfa7
SharifKhalid <sharifkhalid161097@gmail.com>
2022-04-05 17:08:58 +0600

    image added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
         <div>
-            <div>
-                <h1>This is the Product</h1>
-                <p></p>
-            </div>
-            
-            <div>
-                <img src="" alt="" />
+        <div className='home'>
+            <div className='div1'>
+                <h1>Your Product</h1>
+                <li>1.0-type sensor and advanced image processor for stunning images</li>
+                <li>ZEISS® Tessar T* 24mm1 lens for less distortion
+                    Ultra-compact.</li>
+                <li>tough body: waterproof5, dustproof, shockproof6 and crushproof7</li>
             </div>
 
+            <div className='div2'>
+                <img className='image' src="/Photo/damir-babacic-UCfTPw4nyr0-unsplash.jpg" alt="" />
+            </div>
+        </div>
+
+        <button className='btn'>See All Reviews</button>
         </div>
     );
 };
